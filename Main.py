93f2dd9b52a9121804fdf58df2d2a18feea359e5
nbouzidi <najibe.bouzidi@gmail.com>
2016-10-17 00:15:22 +0200

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Crée le 03/07/16
+Dernière modification le 03/07/16
+Author: Najibe BOUZIDI
+"""
+
+import Environnement as Env
+
+
+def main():
+    """
+    Fonction principale
+    """
+    simulation = Env.Environnement()
+    simulation.run()
+
+if __name__ == '__main__':
+    main()
